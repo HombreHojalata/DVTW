899c441ee75028e0683ca111bec6a7e37c28513b
pedro49max <pedrotobalina49@gmail.com>
2026-03-11 15:09:22 +0100

    Se leen las misiones

--- src/manager/missionManager.js
+++ src/manager/missionManager.js
@@ -9,6 +9,11 @@ export default class missionManager{
     constructor(){
         this.player = new Player();
         this.regularMissions = [];
+        this.upMoneyMissions = [];
+        this.upPopularityMissions = [];
+        this.downPopularityMissions = [];
+        this.downMoneyMissions = [];
+        this.downCorruptionMissions = [];
         this.story = new Story();//Aqui se asigna una de las historias de forma aleatoria.
         this.day=new Day();
 
@@ -58,16 +63,129 @@ export default class missionManager{
             this.regularMissions.push(mission);
         });
 
-        /*console.log("Number of missions:", this.missions.length);
-
-        this.missions.forEach(m => {
-            console.log("----");
-            console.log("Mission:", m.name);
-            console.log("Description:", m.description);
+        const responseUM = await fetch("DVTW/files/upMoney.json");
+        const missionsDataUM = await responseUM.json();
+        missionsDataUM.forEach(m => {
+            const mission = new Mission(
+                m.name,
+                m.description,
+                m.event,
+                m.corrupt,
+                m.minigame
+            );
 
             m.options.forEach(o => {
-                console.log("Option:", o.description, "Probability:", o.probability);
+                mission.addOption(
+                    o.probability,
+                    o.description,
+                    o.energy,
+                    o.money,
+                    o.corruption,
+                    o.popularity
+                );
             });
-        });*/
+
+            this.upMoneyMissions.push(mission);
+        });
+
+        const responseUP = await fetch("DVTW/files/upPopularity.json");
+        const missionsDataUP = await responseUP.json();
+        missionsDataUP.forEach(m => {
+            const mission = new Mission(
+                m.name,
+                m.description,
+                m.event,
+                m.corrupt,
+                m.minigame
+            );
+
+            m.options.forEach(o => {
+                mission.addOption(
+                    o.probability,
+                    o.description,
+                    o.energy,
+                    o.money,
+                    o.corruption,
+                    o.popularity
+                );
+            });
+
+            this.upPopularityMissions.push(mission);
+        });
+
+        const responseDP = await fetch("DVTW/files/downPopularity.json");
+        const missionsDataDP = await responseDP.json();
+        missionsDataDP.forEach(m => {
+            const mission = new Mission(
+                m.name,
+                m.description,
+                m.event,
+                m.corrupt,
+                m.minigame
+            );
+
+            m.options.forEach(o => {
+                mission.addOption(
+                    o.probability,
+                    o.description,
+                    o.energy,
+                    o.money,
+                    o.corruption,
+                    o.popularity
+                );
+            });
+
+            this.downPopularityMissions.push(mission);
+        });
+
+        const responseDM = await fetch("DVTW/files/downMoney.json");
+        const missionsDataDM = await responseDM.json();
+        missionsDataDM.forEach(m => {
+            const mission = new Mission(
+                m.name,
+                m.description,
+                m.event,
+                m.corrupt,
+                m.minigame
+            );
+
+            m.options.forEach(o => {
+                mission.addOption(
+                    o.probability,
+                    o.description,
+                    o.energy,
+                    o.money,
+                    o.corruption,
+                    o.popularity
+                );
+            });
+
+            this.downMoneyMissions.push(mission);
+        });
+
+        const responseDC = await fetch("DVTW/files/downCorruption.json");
+        const missionsDataDC = await responseDC.json();
+        missionsDataDC.forEach(m => {
+            const mission = new Mission(
+                m.name,
+                m.description,
+                m.event,
+                m.corrupt,
+                m.minigame
+            );
+
+            m.options.forEach(o => {
+                mission.addOption(
+                    o.probability,
+                    o.description,
+                    o.energy,
+                    o.money,
+                    o.corruption,
+                    o.popularity
+                );
+            });
+
+            this.downCorruptionMissions.push(mission);
+        });
     }
 }
