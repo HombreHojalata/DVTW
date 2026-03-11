c9fad0ed4a775c965adec5c551fcef14cc2f5d9c
JDwanW <149677321+JDwanW@users.noreply.github.com>
2026-03-11 14:51:28 +0100

    creaction .js
    Added:
    Buildings,SpecialBuildings.

---
+++ src/map/building/special/specialBuilding.js
@@ -0,0 +1,10 @@
+export default class SpecialBuilding {
+    constructor(PNGBuilding,coste,beneficio,satisfaccion,energia,poblacion){
+        this.PNGBuilding = PNGBuilding;
+        this.coste = coste;
+        this.beneficio = beneficio;
+        this.satisfaccion = satisfaccion;
+        this.energia = energia;
+        this.poblacion = poblacion;
+    }
+}
--- src/map/district/districtNuevaPradera.js
+++ src/map/district/districtNuevaPradera.js
@@ -1,9 +1,9 @@
 import District from './district';
 
-class DistrictNuevaPradera extends District {
-    constructor(name, desc, population, populationIncrease, satisfaction, district_building, buildings, space_building, special_building, opositors, parameters, parameters_multipliers, PNGwithOutSpecial, PNGwithSpecial, posX, posY ) {
-        super(name, desc, population, populationIncrease, satisfaction, district_building, buildings, space_building, special_building, opositors, parameters, parameters_multipliers, PNGwithOutSpecial, PNGwithSpecial, posX, posY );
+export default class DistrictNuevaPradera extends District {
+    constructor(name, desc, population, populationIncrease, satisfaction, district_building, buildings, space_building, special_building,
+         opositors, parameters, parameters_multipliers, PNGwithOutSpecial, PNGwithSpecial, posX, posY ) {
+        super(name, desc, population, populationIncrease, satisfaction, district_building, buildings, space_building, special_building,
+             opositors, parameters, parameters_multipliers, PNGwithOutSpecial, PNGwithSpecial, posX, posY );
     }
-}
-
-export default DistrictNuevaPradera;
+};
