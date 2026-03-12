9dabfae58bf023eea1607f7e4be624074e2aa6a5
ZLF369 <116487537+ZLF369@users.noreply.github.com>
2026-03-12 22:33:53 +0100

    Update level.js

--- src/level.js
+++ src/level.js
@@ -59,18 +59,97 @@ export default class Level extends Phaser.Scene {
             this.scene.start('configuration', { from: 'level' });
         });
     }
-    spawnPopularityBar(){
-        const barWidth = 400;
-        const barHeight = 25;
-        const barX = 700;
-        const barY = 50;    
-        this.popularityBarBg = this.add.rectangle(barX, barY, barWidth, barHeight, 0x000000).setOrigin(0);
-        this.popularityBarFill = this.add.rectangle(barX, barY, barWidth * (this.player.getPopularity() / 100), barHeight, 0x00ff00).setOrigin(0);
-        this.popularityText = this.add.text(barX + barWidth / 2, barY + barHeight / 2, 'Popularity: ' + Math.floor(this.player.getPopularity()) + '%', { fontSize: '14px', color: '#fff' }).setOrigin(0.5); // He puesto un Math.floor (no necesitamos decimales de porcentaje)
-        this.corruptionBarbg = this.add.rectangle(barX, barY + barHeight + 20, barWidth, barHeight, 0x000000).setOrigin(0);
-        this.corruptionBarFill = this.add.rectangle(barX, barY + barHeight + 20, barWidth * (this.player.getCorruption() / 100), barHeight, 0xff0000).setOrigin(0);
-        this.corruptionText = this.add.text(barX + barWidth / 2, barY + barHeight + 25, 'Corruption: ' + Math.floor(this.player.getCorruption()) + '%', { fontSize: '14px', color: '#fff' }).setOrigin(0.5); // He puesto un Math.floor (no necesitamos decimales de porcentaje)
+
+
+    spawnPopularityBar() {
+        const panelX = 500;
+        const panelY = 12;
+        const panelWidth = 620;
+        const panelHeight = 84;
+
+        this.topStatsBg = this.add.rectangle(panelX, panelY, panelWidth, panelHeight, 0x2b2b2f, 0.96).setOrigin(0);
+        this.topStatsBorder = this.add.rectangle(panelX, panelY, panelWidth, panelHeight).setOrigin(0).setStrokeStyle(3, 0xd2a94e);
+
+        this.topStatsTitle = this.add.text(panelX + 22, panelY + 8, 'PRESIDENTIAL STATUS', {
+            fontSize: '17px',
+            color: '#f3e6c4',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        });
+
+        const barX = panelX + 175;
+        const barY = panelY + 34;
+        const barWidth = 390;
+        const barHeight = 14;
+
+        const popularity = Phaser.Math.Clamp(this.player.getPopularity(), 0, 100);
+        const oppositors = Math.min(15, Math.floor(this.player.getCorruption() * 0.4));
+        const neutrals = Math.max(0, 100 - popularity - oppositors);
+
+        const alliesWidth = barWidth * (popularity / 100);
+        const neutralWidth = barWidth * (neutrals / 100);
+        const oppositorsWidth = barWidth * (oppositors / 100);
+
+        this.popularityLabel = this.add.text(panelX + 22, barY - 1, 'PUBLIC OPINION', {
+            fontSize: '14px',
+            color: '#f5f1e7',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        });
+
+        this.opinionBarBg = this.add.rectangle(barX, barY, barWidth, barHeight, 0x202020).setOrigin(0);
+
+        this.opinionAllies = this.add.rectangle(barX, barY, alliesWidth, barHeight, 0x57c26d).setOrigin(0);
+        this.opinionNeutral = this.add.rectangle(barX + alliesWidth, barY, neutralWidth, barHeight, 0x5f5f5f).setOrigin(0);
+        this.opinionOppositors = this.add.rectangle(barX + alliesWidth + neutralWidth, barY, oppositorsWidth, barHeight, 0xa63a3a).setOrigin(0);
+
+        this.opinionFrame = this.add.rectangle(barX, barY, barWidth, barHeight).setOrigin(0).setStrokeStyle(2, 0xf0dfb6);
+
+        this.opinionPercentText = this.add.text(barX + barWidth / 2, barY + barHeight / 2, Math.floor(popularity) + '%', {
+            fontSize: '12px',
+            color: '#f6f2e8',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        }).setOrigin(0.5);
+
+        this.opinionLegend = this.add.text(barX, barY + 22, 'Allies', {
+            fontSize: '11px',
+            color: '#57c26d',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        });
+
+        this.neutralLegend = this.add.text(barX + 62, barY + 22, 'Neutral', {
+            fontSize: '11px',
+            color: '#d1d1d1',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        });
+
+        this.oppositorLegend = this.add.text(barX + 132, barY + 22, 'Oppositors', {
+            fontSize: '11px',
+            color: '#d85a5a',
+            fontStyle: 'bold',
+            fontFamily: 'Georgia'
+        });
+
+        // this.corruptionLabel = this.add.text(panelX + panelWidth - 150, panelY + panelHeight + 8, 'CORRUPTION', {
+        //     fontSize: '12px',
+        //     color: '#f3d6d6',
+        //     fontStyle: 'bold',
+        //     fontFamily: 'Georgia'
+        // });
+
+        // this.corruptionValue = this.add.text(panelX + panelWidth - 28, panelY + panelHeight + 8, Math.floor(this.player.getCorruption()) + '%', {
+        //     fontSize: '12px',
+        //     color: '#f6f2e8',
+        //     fontStyle: 'bold',
+        //     fontFamily: 'Georgia'
+        // }).setOrigin(1, 0);
     }
+
+
+
     spawnEnergyBar(){   
     //VERTICAL ENERGY BAR ON THE RIGHT SIDE OF THE SCREEN
         const barWidth = 200;
@@ -223,9 +302,11 @@ export default class Level extends Phaser.Scene {
                 this.player.updatePopularity(7);
                 this.player.updateCorruption(12);
                 // WE WILL NEED A FUNC TO UPDATE DATA
-                this.PopularityContainer = this.spawnPopularityBar();
-                this.EnergyBar = this.spawnEnergyBar();
-                this.Footer = this.spawnFooter();
+
+                this.refreshTopStatsPanel();
+                // this.PopularityContainer = this.spawnPopularityBar();
+                // this.EnergyBar = this.spawnEnergyBar();
+                // this.Footer = this.spawnFooter();
             }
             this.missionTypeTwoChoice2.onclick = () => {
                 this.missionTypeTwoChoice1.remove();
@@ -252,6 +333,56 @@ export default class Level extends Phaser.Scene {
         });
 
     }
+
+    refreshTopStatsPanel() {
+        const opinionBarWidth = 390;
+
+        const popularity = Phaser.Math.Clamp(this.player.getPopularity(), 0, 100);
+        const oppositors = Math.min(15, Math.floor(this.player.getCorruption() * 0.4));
+        const neutrals = Math.max(0, 100 - popularity - oppositors);
+
+        const alliesWidth = opinionBarWidth * (popularity / 100);
+        const neutralWidth = opinionBarWidth * (neutrals / 100);
+        const oppositorsWidth = opinionBarWidth * (oppositors / 100);
+
+        if (this.opinionAllies) {
+            this.opinionAllies.width = alliesWidth;
+        }
+
+        if (this.opinionNeutral) {
+            this.opinionNeutral.x = 675 + alliesWidth;
+            this.opinionNeutral.width = neutralWidth;
+        }
+
+        if (this.opinionOppositors) {
+            this.opinionOppositors.x = 675 + alliesWidth + neutralWidth;
+            this.opinionOppositors.width = oppositorsWidth;
+        }
+
+        if (this.opinionPercentText) {
+            this.opinionPercentText.setText(Math.floor(popularity) + '%');
+        }
+
+        if (this.corruptionValue) {
+            this.corruptionValue.setText(Math.floor(this.player.getCorruption()) + '%');
+        }
+
+        if (this.moneyText) {
+            this.moneyText.setText(this.player.getMoney() + '$');
+        }
+
+        if (this.energyBarFill) {
+            const barHeight = 500;
+            this.energyBarFill.y = 150 + barHeight * (1 - this.player.getEnergy() / 100);
+            this.energyBarFill.height = barHeight * (this.player.getEnergy() / 100);
+        }
+
+        if (this.energyText) {
+            this.energyText.setText('Energy: ' + Math.floor(this.player.getEnergy()) + '%');
+        }
+    }
+
+
     updateDistrictFooter(district) {
         this.districtTitleText.setText(district.getName());
     }
