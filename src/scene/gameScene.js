68e7365e7cd20edd803328d81cb7bb19faa16edf
Manuel del Caño <146079243+HombreHojalata@users.noreply.github.com>
2026-03-25 00:14:48 +0100

    Cambiar el tiempo de cada día

--- src/scene/gameScene.js
+++ src/scene/gameScene.js
@@ -69,7 +69,7 @@ export default class GameScene extends Phaser.Scene {
     */
 
     startEnergyDrain() {
-        this.totalDayDurationMs = 0.01 * 60 * 1000;
+        this.totalDayDurationMs = 8 * 60 * 1000;
         this.energyTickMs = 250;
         const maxEnergy = this.player.getMaxEnergy() || 100;
         this.energyDrainPerTick = maxEnergy / (this.totalDayDurationMs / this.energyTickMs);
