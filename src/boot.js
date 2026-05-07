5883a2d420528e506f02c8ef3aa7e98278acb667
pedro49max <pedrotobalina49@gmail.com>
2026-05-07 15:01:32 +0200

    Update boot.js

--- src/boot.js
+++ src/boot.js
@@ -576,7 +576,7 @@ export default class Boot extends Phaser.Scene {
     this.cache.json.add('whacJSON15', whacJSON15);
 
     //AUDIO PART
-    /*this.load.audio('quack', quackAudio);
+    this.load.audio('quack', quackAudio);
     this.load.audio('blackMarketAudio', blackMarketAudio);
     this.load.audio('introSceneAudio', introSceneAudio);
     this.load.audio('bgMusic', gameAudio);
@@ -602,7 +602,7 @@ export default class Boot extends Phaser.Scene {
     this.load.audio('coin6', coin6);
     this.load.audio('coin7', coin7);
     this.load.audio('coin8', coin8);
-    this.load.audio('coin9', coin9);*/
+    this.load.audio('coin9', coin9);
     this.load.audio('coin10', coin10);
     this.load.audio('coin11', coin11);
     this.load.audio('coin12', coin12);
