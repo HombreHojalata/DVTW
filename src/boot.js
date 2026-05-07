8d95966b6c6dba53e08fecda9dc4fa1cbf866e31
pedro49max <pedrotobalina49@gmail.com>
2026-05-07 15:20:34 +0200

    8

--- src/boot.js
+++ src/boot.js
@@ -610,8 +610,10 @@ export default class Boot extends Phaser.Scene {
     this.load.audio('moneyBag', moneyBag);
 
     this.load.audio('flam1', flam1);
-    /*this.load.audio('flam2', flam2);
-    this.load.audio('flam3', flam3);*/
+
+    
+    this.load.audio('flam2', flam2);
+    //this.load.audio('flam3', flam3);
     
     this.load.audio('flam4', flam4);
     this.load.audio('flam5', flam5);
